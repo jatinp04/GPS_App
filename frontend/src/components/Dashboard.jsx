78fdc6dd2e9cbe06085afcf63eed8f2757c7737e
Jatin <jatinpatel292@gmail.com>
2023-02-25 17:36:11 +0530

    dyanmic color in pie chart

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { React, useState, useEffect } from "react";
-import { useNavigate, useParams ,useLocation} from "react-router-dom";
+import { useNavigate, useParams, useLocation } from "react-router-dom";
 import "./style/Dashboard.css";
 // import axios from "axios";
 import axios from "../Api";
@@ -12,24 +12,32 @@ function Dashboard() {
   const [getPieData, setPieData] = useState([]);
   const location = useLocation();
   const dev_id = useParams().dev_id;
-  console.log(dev_id);
-  console.log(useParams());
+  // console.log(dev_id);
+  // console.log(useParams());
 
-  
+  const piecolor = [
+    "#E38627","#C13C37","#6A2135","#C13C37","#E38627","#6A2135"
+  ];
 
   const dataMock = [
     { title: "One", value: 10, color: "#E38627" },
     { title: "Two", value: 15, color: "#C13C37" },
     { title: "Three", value: 20, color: "#6A2135" },
   ];
+ let index = 0;
 
   function formatToPieChartData(apiData) {
     const piedata = [];
-
     const grouped = _.groupBy(apiData, "latest_location");
     _.map(grouped, (value, key) => {
       // piedata.push({title: key, value: value.length, color: `#C13C37`});
-      piedata.push({title: key, value: value.length, color: `#DF${value.length}63C`});
+    
+      piedata.push({
+        title: key,
+        value: value.length,
+        
+        color: piecolor[index++],
+      });
     });
 
     console.log("piedate: ", piedata);
@@ -37,11 +45,13 @@ function Dashboard() {
   }
 
   function getAllDevices(dev_id) {
-    axios.get(`/devices?dev_id=${dev_id}`,{withCredentials:true}).then((response) => {
-      setDevices(response.data.results);
-      // console.log(setDevices);
-      formatToPieChartData(response.data.results);
-    });
+    axios
+      .get(`/devices?dev_id=${dev_id}`, { withCredentials: true })
+      .then((response) => {
+        setDevices(response.data.results);
+        // console.log(setDevices);
+        formatToPieChartData(response.data.results);
+      });
   }
   useEffect(() => {
     getAllDevices(location.state.search);
@@ -49,7 +59,7 @@ function Dashboard() {
 
   return (
     <>
-    <NavBar></NavBar>
+      <NavBar></NavBar>
       <div style={{ display: "flex", float: "left", width: "100%" }}>
         <div className="container-tableD">
           <table>
@@ -70,20 +80,16 @@ function Dashboard() {
           </table>
         </div>
         <div style={{ width: "50%", marginRight: "40px" }}>
-         
           <PieChart
             data={getPieData}
             radius="15"
             segmentsShift={(index) => (index === 0 ? 3 : 0)}
-            label={({ dataEntry }) =>( dataEntry.percentage+"%")}
-
+            label={({ dataEntry }) => dataEntry.percentage + "%"}
             labelStyle={{
               fontSize: "2.5px",
-              fontFamily:"inherit",
-               
+              fontFamily: "inherit",
             }}
           />
-           
         </div>
       </div>
     </>
